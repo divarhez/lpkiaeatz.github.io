3261dd4a2a9a89903904c51efde36cd6471e8fcd
divarhez <divaraihanr@gmail.com>
2025-05-23 18:21:14 +0700

    revisi3

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::get('/cart', [MenuController::class, 'showCart'])->name('cart.show');
 Route::post('/cart/update/{id}', [MenuController::class, 'updateCart'])->name('cart.update');
 Route::get('/cart/remove/{id}', [MenuController::class, 'removeFromCart'])->name('cart.remove');
 Route::post('/checkout', [MenuController::class, 'checkout'])->name('checkout');
+Route::get('/search', [MenuController::class, 'search'])->name('menu.search');
