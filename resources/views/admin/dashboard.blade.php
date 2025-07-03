558bc4374611c2603ad5e1f15d5bfafbd5595728
divarhez <divaraihanr@gmail.com>
2025-07-03 12:29:02 +0700

    hapusmenu

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,10 +4,6 @@
 <div class="container mx-auto px-6 py-12 max-w-4xl">
     <h2 class="text-3xl font-bold text-[#FF914D] mb-8 text-center">Dashboard Admin</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-10">
-        <a href="{{ route('stok-makanan.index') }}" class="block bg-white rounded-xl shadow p-8 border border-[#FFD6A5] hover:shadow-amber-200 hover:-translate-y-2 hover:scale-105 transition-all duration-300 text-center">
-            <div class="text-4xl mb-2">🍱</div>
-            <div class="font-bold text-lg text-[#FF914D]">Manajemen Stok Makanan</div>
-        </a>
         <a href="{{ route('orders.history', ['filter' => 'day']) }}" class="block bg-white rounded-xl shadow p-8 border border-[#FFD6A5] hover:shadow-amber-200 hover:-translate-y-2 hover:scale-105 transition-all duration-300 text-center">
             <div class="text-4xl mb-2">📊</div>
             <div class="font-bold text-lg text-[#FF914D]">Laporan Penjualan</div>
